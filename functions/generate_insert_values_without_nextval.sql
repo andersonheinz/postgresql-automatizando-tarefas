23f46f098ebb2cf9102bc843fad0c7e7607a2afc
Anderson Heinz <59366261+andersonheinz@users.noreply.github.com>
2020-04-24 10:01:31 -0300

    Update generate_insert_values_without_nextval.sql

--- functions/generate_insert_values_without_nextval.sql
+++ functions/generate_insert_values_without_nextval.sql
@@ -17,9 +17,9 @@ DECLARE
 BEGIN
     SELECT
         INTO v_fields ARRAY_TO_STRING(ARRAY (
-SELECT COLUMN_NAME::varchar(50) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = _table
-AND COLUMN_NAME NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name = _table
-AND column_default ILIKE 'nextval%') ORDER BY ORDINAL_POSITION), ', ');
+            SELECT COLUMN_NAME::varchar(50) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = _table
+            AND COLUMN_NAME NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name = _table
+            AND column_default ILIKE 'nextval%') ORDER BY ORDINAL_POSITION), ', ');
     SELECT
         INTO v_output 'INSERT INTO ' || _table || '(' || v_fields || ') SELECT ' || v_fields || ' FROM '|| _table ||';';
     RETURN v_output;
